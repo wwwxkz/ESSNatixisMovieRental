41a3cfcaa991aa6e1355245f81a32e72be0bad81
ESSNatixis <essnatixis@gmail.com>
2025-05-20 10:15:16 +0100

    Init

---
+++ MovieRental/Program.cs
@@ -0,0 +1,33 @@
+using MovieRental.Data;
+using MovieRental.Movie;
+using MovieRental.Rental;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEntityFrameworkSqlite().AddDbContext<MovieRentalDbContext>();
+
+builder.Services.AddSingleton<IRentalFeatures, RentalFeatures>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using (var client = new MovieRentalDbContext())
+{
+	client.Database.EnsureCreated();
+}
+
+app.Run();
