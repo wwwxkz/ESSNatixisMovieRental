ca501b1c354482587277cafbbc464a8a856a7964
wwwxkz <wxkzwin@gmail.com>
2025-10-22 05:30:15 -0300

    Removed unecessary comments and duplicate files in domain

--- MovieRental.Domain/Interfaces/IMovieFeatures.cs
+++ MovieRental.Domain/Interfaces/IMovieFeatures.cs
@@ -4,23 +4,11 @@ namespace MovieRental.Domain.Interfaces;
 
 public interface IMovieFeatures
 {
-    /// <summary>
-    /// Saves a movie to the database
-    /// </summary>
     Task<Movie> SaveAsync(Movie movie, CancellationToken cancellationToken = default);
 
-    /// <summary>
-    /// Gets all movies ordered by title
-    /// </summary>
     Task<IEnumerable<Movie>> GetAllAsync(CancellationToken cancellationToken = default);
     
-    /// <summary>
-    /// Gets a movie by its ID
-    /// </summary>
     Task<Movie?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
     
-    /// <summary>
-    /// Adds a new movie to the database
-    /// </summary>
     Task<Movie> AddAsync(Movie movie, CancellationToken cancellationToken = default);
 }
